df9797a9a0be17552361b7c37b44448337e5b34a
al3xhh <al3xhh1995@gmail.com>
2018-03-13 15:24:12 +0100

    Added tournament selection.

--- src/Selection/SelectionFactory.java
+++ src/Selection/SelectionFactory.java
@@ -4,6 +4,8 @@ public class SelectionFactory {
 	public static SelectionAlgorithm getSelectionAlgorithm(String algorithm, int participantes) {
 		if (algorithm.equals("Roulette"))
 			return new Roulette();
+		else if (algorithm.equals("Tournament"))
+			return new Tournament();
 		else
 			return new Roulette();
 	}
